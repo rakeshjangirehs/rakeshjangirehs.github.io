790951d8d4a7768a633c15050841493f2a759377
Rakesh Jangir <rakeshj@ehealth.local>
2020-01-23 16:32:48 +0530

    Video 5 Completed

---
+++ assets/js/functions.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".toggled-nav-wrapper").on("click",function(e){
+        e.stopPropagation();
+
+    });
+});
+
+$(".nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay").on("click", function(e){
+    $("html").toggleClass("nav-open");
+    console.log('hi');
+});
